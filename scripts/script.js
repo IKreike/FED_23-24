12064888a1c368f05469bbd8d72a8a1c4e076a52
IKreike <iris.kreike@hva.nl>
2023-12-07 11:37:08 +0100

    v0.7
    7/12/23
    over ons pagina: artikelen en colapsable

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,36 @@
 // JavaScript Document
 console.log("hi");
+
+
+
+
+// collapsible working?
+// bron: https://www.w3schools.com/howto/howto_js_collapsible.asp
+// var coll = document.getElementsByClassName("collapsible");
+// var i;
+
+// console.log(coll);
+
+// for (i = 0; i < coll.length; i++) {
+//   coll[i].addEventListener("click", function() {
+//     this.classList.toggle("active");
+
+//     var contentCollapsible = this.nextElementSibling;
+//     if (contentCollapsible.style.display === "block") {
+//         contentCollapsible.style.display = "none";
+//     } else {
+//         contentCollapsible.style.display = "block";
+//     }
+//   });
+// }
+
+// end copyright
+var fold = document.getElementById("collapsible");
+var inhoud = document.getElementById("contentCollapsible");
+
+fold.addEventListener("click", displaycontent);
+
+function displaycontent(){
+    console.log("shit works, surprisingly")
+    inhoud.classList.toggle("hide")
+}
